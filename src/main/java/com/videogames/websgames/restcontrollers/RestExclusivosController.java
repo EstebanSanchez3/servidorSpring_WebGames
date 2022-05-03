f2ddf7be0519e876ce47d00d27bc4ec1a854a68a
EstebanSanchez3 <esteban2324.sanchez@gmail.com>
2022-05-03 12:52:34 +0200

    primer commit
    primer commit con entities, repository, service, controlloer y restcontroler

--- src/main/java/com/videogames/websgames/restcontrollers/RestExclusivosController.java
+++ src/main/java/com/videogames/websgames/restcontrollers/RestExclusivosController.java
@@ -1,8 +1,26 @@
 package com.videogames.websgames.restcontrollers;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.videogames.websgames.entities.ExclusivosEntities;
+import com.videogames.websgames.services.IExclusivosService;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
 public class RestExclusivosController {
+	@Autowired
+	IExclusivosService ExclusivosService ;
+	@GetMapping(path="/exclusivos", produces=
+		{MediaType.APPLICATION_JSON_VALUE})
+	List<ExclusivosEntities> getexclusivos(){
+		return ExclusivosService.findAll();
 
+}
 }
